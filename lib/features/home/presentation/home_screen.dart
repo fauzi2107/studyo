f9767ae135e3fa5d83776d04dc20d340a2af1a8c
Fauzi <fauzi.nur21@gmail.com>
2024-10-04 16:46:41 +0700

    add random numerator and denominator

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,5 +1,3 @@
-import 'package:studyo/features/painter/triangle/enum_triangle_direction.dart';
-
 import '../../../ui_export.dart';
 
 part 'home_state.dart';
@@ -12,51 +10,110 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomePageState extends HomeState<HomeScreen> {
+  int numerator = 1, denominator = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            InteractiveTriangleDownward(
-              position: horizontalPosition,
-              onMovePointer: onUpdateHorizontalPointer,
-              onChangeColumn: onUpdateColumn,
-              direction: TriangleDirection.downward,
-              columns: columns,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InteractiveTriangleRightward(
-                  rows: rows,
-                  position: verticalPosition,
-                  onMovePointer: onUpdateVerticalPointer,
-                  onChangeRow: onUpdateRows,
-                  direction: TriangleDirection.rightward,
-                ),
-                InteractiveRectangles(
-                  totalColumn: columns,
-                  totalRow: rows,
-                ),
-                InteractiveTriangleLeftward(
-                  rows: rows,
-                  position: verticalPosition,
-                  onMovePointer: onUpdateVerticalPointer,
-                  onChangeRow: onUpdateRows,
-                  direction: TriangleDirection.leftward,
-                ),
-              ],
-            ),
-            InteractiveTriangleUpward(
-              columns: columns,
-              direction: TriangleDirection.upward,
-              position: horizontalPosition,
-              onMovePointer: onUpdateHorizontalPointer,
-              onChangeColumn: onUpdateColumn,
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  numerator = Random().nextInt(100) + 1;
+                  denominator = Random().nextInt(100) + 1;
+                });
+              },
+              icon: const Icon(Icons.refresh_rounded),
+              color: Colors.grey,
             )
           ],
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 20
+          ),
+          child: Column(
+            children: [
+              Text('$numerator'),
+              SizedBox(
+                width: 25,
+                child: Divider(
+                  color: Colors.grey,
+                  height: 1,
+                ),
+              ),
+              Text('$denominator'),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InteractiveTriangleDownward(
+                      position: horizontalPosition,
+                      onMovePointer: onUpdateHorizontalPointer,
+                      onChangeColumn: onUpdateColumn,
+                      direction: TriangleDirection.downward,
+                      columns: columns,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InteractiveTriangleRightward(
+                          rows: rows,
+                          position: verticalPosition,
+                          onMovePointer: onUpdateVerticalPointer,
+                          onChangeRow: onUpdateRows,
+                          direction: TriangleDirection.rightward,
+                        ),
+                        InteractiveRectangles(
+                          totalColumn: columns,
+                          totalRow: rows,
+                        ),
+                        InteractiveTriangleLeftward(
+                          rows: rows,
+                          position: verticalPosition,
+                          onMovePointer: onUpdateVerticalPointer,
+                          onChangeRow: onUpdateRows,
+                          direction: TriangleDirection.leftward,
+                        ),
+                      ],
+                    ),
+                    InteractiveTriangleUpward(
+                      columns: columns,
+                      direction: TriangleDirection.upward,
+                      position: horizontalPosition,
+                      onMovePointer: onUpdateHorizontalPointer,
+                      onChangeColumn: onUpdateColumn,
+                    )
+                  ],
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {
+
+                },
+                color: Colors.grey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                elevation: 0,
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                padding: EdgeInsets.zero,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: const Icon(Icons.check,
+                    color: Colors.white,
+                    size: 32,
+                  )
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
